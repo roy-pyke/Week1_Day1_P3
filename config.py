f1ca3122c39e8b67b3c816c981533da1a0f95136
rw <woerzina@gmail.com>
2025-03-21 19:56:53 -0700

    3/21 19:56 添加了.env

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 import os
+from dotenv import load_dotenv
+
+#加载项目根目录下的 .env文件
+load_dotenv()
 
 
 class Config:
     # 秘钥建议通过环境变量传入，保证安全性
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret_key')
 
     # 数据库 URI 同样通过环境变量配置，默认使用 SQLite
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///students.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///students.db')
 
     # 禁用 SQLAlchemy 的事件系统，减少不必要的开销
     SQLALCHEMY_TRACK_MODIFICATIONS = False
